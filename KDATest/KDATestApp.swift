aaf438ea9c084126c9f3120a994bc4adc109dbd3
elmysf <elmysufiandy09@gmail.com>
2023-11-06 18:23:59 +0700

    Initial Commit

---
+++ KDATest/KDATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KDATestApp.swift
+//  KDATest
+//
+//  Created by Sufiandy Elmy on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct KDATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
