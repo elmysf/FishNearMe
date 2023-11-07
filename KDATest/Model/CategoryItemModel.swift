83cce0017c337088a5fc70b4ed3e6600eaf7ac07
elmysf <elmysufiandy09@gmail.com>
2023-11-07 14:01:46 +0700

    Move File

---
+++ KDATest/Model/CategoryItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryItemModel.swift
+//  KDATest
+//
+//  Created by Sufiandy Elmy on 07/11/23.
+//
+
+import Foundation
---
+++ KDATest/resources/network/Repository.swift
@@ -0,0 +1,8 @@
+//
+//  Repository.swift
+//  KDATest
+//
+//  Created by Sufiandy Elmy on 07/11/23.
+//
+
+import Foundation
