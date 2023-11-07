74d637b9c7666b87a4aa945363a279b5e62c4fb1
elmysf <elmysufiandy09@gmail.com>
2023-11-07 14:19:42 +0700

    Add RemoteImageURL

--- KDATest/resources/utils/RemoteImageURL.swift
+++ KDATest/resources/utils/RemoteImageURL.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+//load remote image
+class RemoteImageURL:  ObservableObject {
+    @Published var data = Data()
+    
+    init(imageUrl: String) {
+        guard let url = URL(string: imageUrl) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }.resume()
+    }
+}
+
