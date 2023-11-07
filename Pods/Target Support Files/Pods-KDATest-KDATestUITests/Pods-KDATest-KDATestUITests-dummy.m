2dd8737d0261b660a35ade378ca3393780e78049
elmysf <elmysufiandy09@gmail.com>
2023-11-07 12:40:12 +0700

    Add Podfile

---
+++ Pods/Target Support Files/Pods-KDATest-KDATestUITests/Pods-KDATest-KDATestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KDATest_KDATestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KDATest_KDATestUITests
+@end
---
+++ Pods/Target Support Files/Pods-KDATest/Pods-KDATest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KDATest : NSObject
+@end
+@implementation PodsDummy_Pods_KDATest
+@end
